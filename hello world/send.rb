c11023fe45fdd805cc1743bbb9430257cf997275
wrudnicki16 <wyattrudnicki@gmail.com>
2025-10-22 14:17:14 -0700

    Add first few tutorials

---
+++ hello world/send.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'bunny'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+
+queue = channel.queue('hello')
+
+channel.default_exchange.publish('Hello World!', routing_key: queue.name)
+puts " [x] Sent 'Hello World!'"
+
+connection.close
---
+++ work queues/new_task.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'bunny'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+
+queue = channel.queue('task_queue', durable: true)
+
+message = ARGV.empty? ? 'Hello World!' : ARGV.join(' ')
+
+queue.publish(message, persistent: true)
+puts " [x] Sent #{message}"
+
+connection.close
+
+
